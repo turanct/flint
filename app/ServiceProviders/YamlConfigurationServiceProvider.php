c0c6db7a5f0017066bc30ca0fd6a2a834030a6fa
turanct <spinnewebber_toon@hotmail.com>
2014-05-05 23:58:22 +0200

    Make the YAML config service provider cache to php files on disk

--- app/ServiceProviders/YamlConfigurationServiceProvider.php
+++ app/ServiceProviders/YamlConfigurationServiceProvider.php
@@ -19,6 +19,21 @@ use Symfony\Component\Yaml\Yaml;
 class YamlConfigurationServiceProvider implements ServiceProviderInterface
 {
     /**
+     * @var string The path to the live file
+     */
+    protected $liveFile;
+
+    /**
+     * @var string The path to the cached file
+     */
+    protected $cacheFile;
+
+    /**
+     * @var bool Should we cache to disk?
+     */
+    protected $cacheToDisk;
+
+    /**
      * Register method
      *
      * This method will be called by Silex\Application when it's time to register
@@ -27,15 +42,70 @@ class YamlConfigurationServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
-        // Load YAML configs
-        $yaml = file_get_contents($app['base_dir'] . '/app/config.yml');
-        $yaml = str_replace('__BASEDIR__', $app['base_dir'], $yaml);
+        $this->liveFile = $app['base_dir'] . '/app/config.yml';
 
-        $configurations = Yaml::parse($yaml);
+        $this->cacheFile = $app['base_dir'] . '/app/Cache/config.php';
+        if (!is_writable($this->cacheFile)) {
+            $this->cacheFile = '/tmp/silex/cache/config.php';
+        }
 
-        foreach ($configurations as $key => $value) {
+        $this->cacheToDisk = true;
+
+        $configuration = $this->getConfiguration($app);
+
+        foreach ($configuration as $key => $value) {
             $app[$key] = $value;
         }
+    }
+
+    /**
+     * Get the config parameters
+     *
+     * @param Silex\Application $app The app instance
+     *
+     * @return array An array of parameters
+     */
+    public function getConfiguration(Application $app)
+    {
+        if (
+            $this->cacheToDisk === true
+            && file_exists($this->cacheFile)
+            && filemtime($this->cacheFile) > filemtime($this->liveFile)
+        ) {
+            $configuration = include($this->cacheFile);
+        } else {
+            $configuration = $this->getYamlConfiguration($app, $this->cacheToDisk);
+        }
+
+        return $configuration;
+    }
+
+    /**
+     * Get the config parameters from a yaml file
+     *
+     * @param Silex\Application $app         The app instance
+     * @param bool              $cacheToDisk Should we cache the result to disk?
+     *
+     * @return array An array of parameters
+     */
+    public function getYamlConfiguration(Application $app, $cacheToDisk = true)
+    {
+        // Load YAML configs
+        $yaml = file_get_contents($this->liveFile);
+        $yaml = str_replace('__BASEDIR__', $app['base_dir'], $yaml);
+
+        $configuration = Yaml::parse($yaml);
+
+        if ($cacheToDisk === true) {
+            $cache = sprintf(
+                '<?php return %s;',
+                var_export($configuration, true)
+            );
+
+            file_put_contents($this->cacheFile, $cache);
+        }
+
+        return $configuration;
     }
 
     /**
